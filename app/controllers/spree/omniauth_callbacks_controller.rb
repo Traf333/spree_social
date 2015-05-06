5a23825b5e056f2e98de93664901069a754e34bc
igor <igortr333@gmail.com>
2015-05-06 16:44:40 +1000

    fix gemspec hide overrides

--- app/controllers/spree/omniauth_callbacks_controller.rb
+++ app/controllers/spree/omniauth_callbacks_controller.rb
@@ -29,6 +29,7 @@ class Spree::OmniauthCallbacksController < Devise::OmniauthCallbacksController
             user = Spree::User.find_by_email(auth_hash['info']['email']) || Spree::User.new
             user.apply_omniauth(auth_hash)
             if user.save
+              user.add_tokens_from_omniauth
               flash[:notice] = I18n.t("devise.omniauth_callbacks.success", :kind => auth_hash['provider'])
               sign_in_and_redirect :spree_user, user
             else
@@ -65,4 +66,9 @@ class Spree::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def auth_hash
     request.env["omniauth.auth"]
   end
+
+  def current_store
+    Spree::Store.first
+  end
+
 end
--- spree_social.gemspec
+++ spree_social.gemspec
@@ -1,10 +1,10 @@
 Gem::Specification.new do |s|
   s.platform    = Gem::Platform::RUBY
   s.name        = 'spree_social'
-  s.version     = '2.3.0'
+  s.version     = '2.4.0'
   s.summary     = 'Adds social network login services (OAuth) to Spree'
   s.description = s.summary
-  s.required_ruby_version = '>= 1.9.3'
+  s.required_ruby_version = '>= 2.1.0'
 
   s.author            = 'John Dyer'
   s.email             = 'jdyer@spreecommerce.com'
